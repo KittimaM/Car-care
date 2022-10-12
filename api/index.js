527d37491c3b0e053aedc69c5cfe3580faf4804d
Kittima Moolamart <riutyjku@hotmail.com>
2022-10-13 01:44:37 +0700

    install express cores in api

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
-import React from 'react'
-
-const index = () => {
-  return (
-    <div>test main edit</div>
-  )
-}
-
-export default index
+var express = require('express')
+var cors = require('cors')
+var app = express()
+ 
+app.use(cors())
+ 
+app.get('/', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+ 
+app.listen(7879, function () {
+  console.log('CORS-enabled web server listening on port 7879')
+})

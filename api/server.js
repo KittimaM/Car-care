22c76a836d168b5d328fffa18225e88a4ec076f5
Kittima Moolamart <riutyjku@hotmail.com>
2022-10-19 02:15:36 +0700

    connected api and db

---
+++ api/server.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+
+const mysql = require('mysql2');
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'carcare',
+  password: 'carcare'
+});
+
+app.use(cors())
+
+app.get('/', function (req, res, next) {
+    connection.query(
+        'SELECT * FROM `menu` ',
+        function(err, results, fields) {
+          res.json(results); // results contains rows returned by server
+         
+        }
+      );
+})
+
+app.listen(5000, function () {
+  console.log('CORS-enabled web server listening on port 5000')
+})

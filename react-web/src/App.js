8efefce9f4593dbed3bcd3b3475d51f83d8f9adf
Kittima Moolamart <riutyjku@hotmail.com>
2022-11-06 01:03:48 +0700

    add axios react-router

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-import "./App.css";
-
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./Layout/Customer/Login";
+import Home from "./Layout/Home";
+import Navbar from "./Layout/Navbar/Navbar";
 function App() {
-
   return (
-    <div>
-     first Page
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/login" exact><Login /></Route>
+        <Route path="/" exact><Home /></Route>
+        <Route path="/signup"></Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ react-web/src/Layout/Customer/Login.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Login = () => {
+    const [data , setData] = useState()
+
+    useEffect(async () => {
+        const result = await axios('')
+    })
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login

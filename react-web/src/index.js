abc4ebb9b80313b21539b87d1b2c96263997b928
Kittima Moolamart <riutyjku@hotmail.com>
2022-12-30 16:44:42 +0700

    add bootstrap

--- react-web/src/index.js
+++ react-web/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import Index from './Layouts/Navbar/Index'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 

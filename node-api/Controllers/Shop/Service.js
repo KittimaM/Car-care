10698dda43feefed88498d29bc740f5a8df9a5af
Kittima Moolamart <riutyjku@hotmail.com>
2023-02-28 17:48:46 +0700

    commit

--- node-api/Controllers/Shop/Service.js
+++ node-api/Controllers/Shop/Service.js
@@ -4,17 +4,17 @@ const Conn = require("../../db");
 const AddCarSize = (req, res, next) => {
   const { car_size } = req.body;
 
-  Conn.execute(
-    `INSERT INTO carsize(car_size) VALUE (?) `,
-    [car_size],
-    function (err, result) {
-      if (err) {
-        res.json({ status: "ERROR", msg: err });
-      } else {
-        res.json({ status: "OK", msg: result });
-      }
-    }
-  );
+  // Conn.execute(
+  //   `INSERT INTO carsize(car_size) VALUE (?) `,
+  //   [car_size],
+  //   function (err, result) {
+  //     if (err) {
+  //       res.json({ status: "ERROR", msg: err });
+  //     } else {
+  //       res.json({ status: "OK", msg: result });
+  //     }
+  //   }
+  // );
 };
 
 exports.AddCarSize = AddCarSize;
